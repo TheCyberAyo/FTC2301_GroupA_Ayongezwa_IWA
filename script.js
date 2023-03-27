4db335d1f9e1819af99c4c6d867b17d0c62feacd
TheCyberAyo <ndamaseayongezwa@gmail.com>
2023-03-27 17:36:47 +0200

    corvet string/word to interger/number

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add =document.querySelector('[data-key="add"]')
 
+const subtractHandler = () => {
+    const newValue = parseInt (number.value) - 1 
+    number.value = newValue
 
-subtract.addEventListener
 
-add.addEventListener
+}
 
-console.log(number, subtract, add)
+const addHandler = () => {
+    const newValue = parseInt (number.value) + 1 
+    number.value = newValue
+
+}
+
+
+subtract.addEventListener('click', subtractHandler)
+
+add.addEventListener('click', addHandler)
