662b470d8a209271344a66f4329ed48c67d9b708
TheCyberAyo <ndamaseayongezwa@gmail.com>
2023-03-27 16:17:36 +0200

    added const number, subtract and add

---
+++ script.js
@@ -0,0 +1,10 @@
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subtract"]')
+const add =document.querySelector('[data-key="add"]')
+
+
+subtract.addEventListener
+
+add.addEventListener
+
+console.log(number, subtract, add)
